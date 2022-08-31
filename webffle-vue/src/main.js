9638129030ac34da4a98f420606202cc59e93ff6
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-08-31 17:48:18 +0900

    After i will add vue-router

--- webffle-vue/src/main.js
+++ webffle-vue/src/main.js
@@ -7,21 +7,4 @@ import MainSlider from './components/slider-section'
 import i18n from './i18n'
 
 createApp(App).use(i18n).component(MainSlider.name, MainSlider)
-
-// Footer
-import Footer from './components/footer.vue'
-createApp(App).use(i18n).component(Footer.name, Footer)
-
-import mainSubInfo from './components/main-subinfo.vue'
-createApp(App).use(i18n).component(mainSubInfo.name, mainSubInfo)
-
-import mainIntro from './components/main-intro.vue'
-createApp(App).use(i18n).component(mainIntro.name, mainIntro)
-
-import mainOthers from './components/main-otherpages'
-createApp(App).use(i18n).component(mainOthers.name, mainOthers)
-
-import mainAbout from './components/main-about'
-createApp(App).use(i18n).component(mainAbout.name, mainAbout)
-
 createApp(App).use(i18n).mount('#app')
